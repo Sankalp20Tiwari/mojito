62d64e316b4e506ede1f790eadb3c55e376d0f08
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-07-01 14:27:56 +0530

    list data

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,3 +70,18 @@ export const mockTailLists = [
 	price: "$29",
  },
 ];
+
+export const featureLists = [
+ "Perfectly balanced blends",
+ "Garnished to perfection",
+ "Ice-cold every time",
+ "Expertly shaken & stirred",
+];
+
+export const goodLists = [
+ "Handpicked ingredients",
+ "Signature techniques",
+ "Bartending artistry in action",
+ "Freshly muddled flavors",
+];
+
