f42ebc2ef435cbdad869f6955c0291c381354d64
Sankalp Tiwari <sankalp20tiwari@gmail.com>
2025-07-02 15:47:47 +0530

    contact data

--- src/constants/index.js
+++ src/constants/index.js
@@ -119,3 +119,28 @@ export const allCocktails = [
 	 "Each cocktail is made with fresh ingredients and a passion for perfecting every pour, whether you're celebrating or simply relaxing.",
  },
 ];
+
+export const openingHours = [
+ { day: "Mon–Thu", time: "11:00am – 12am" },
+ { day: "Fri", time: "11:00am – 2am" },
+ { day: "Sat", time: "9:00am – 2am" },
+ { day: "Sun", time: "9:00am – 1am" },
+];
+
+export const socials = [
+ {
+	name: "Instagram",
+	icon: "/images/insta.png",
+	url: "#",
+ },
+ {
+	name: "X (Twitter)",
+	icon: "/images/x.png",
+	url: "#",
+ },
+ {
+	name: "Facebook",
+	icon: "/images/fb.png",
+	url: "#",
+ },
+];
